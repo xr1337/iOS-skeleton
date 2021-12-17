39c19f2ae724d2d41673d829a650678b6b6b50fd
Sufiyan Yasa <x1337@gmail.com>
2021-12-18 07:54:08 +0800

    Add common util files that i commonly use

---
+++ App/Utils/Bundle+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Bundle+Extension.swift
+//  ReaderSafari
+//
+//  Created by Sufiyan Yasa on 01/12/2021.
+//
+
+import Foundation
+import UIKit
+
+extension Bundle {
+  
+  public var icon: UIImage? {
+    if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+       let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+       let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+       let lastIcon = iconFiles.last {
+      return UIImage(named: lastIcon)
+    }
+    return nil
+  }
+}
---
+++ App/Utils/UIView+Autolayout.swift
@@ -0,0 +1,58 @@
+//
+//  UIView+Autolayout.swift
+//  ReaderSafari
+//
+//  Created by sufiyan yasa on 21/11/2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+  @discardableResult
+  func anchorByLanguage(
+    top: NSLayoutYAxisAnchor? = nil,
+    leading: NSLayoutXAxisAnchor? = nil,
+    bottom: NSLayoutYAxisAnchor? = nil,
+    trailing: NSLayoutXAxisAnchor? = nil,
+    topConstant: CGFloat = 0,
+    leadingConstant: CGFloat = 0,
+    bottomConstant: CGFloat = 0,
+    trailingConstant: CGFloat = 0,
+    widthConstant: CGFloat = 0,
+    heightConstant: CGFloat = 0) -> [NSLayoutConstraint] {
+      // https://videos.letsbuildthatapp.com/
+      translatesAutoresizingMaskIntoConstraints = false
+
+      var anchors = [NSLayoutConstraint]()
+
+      if let top = top {
+        anchors.append(topAnchor.constraint(equalTo: top, constant: topConstant))
+      }
+
+      if let leading = leading {
+        anchors.append(leadingAnchor.constraint(equalTo: leading, constant: leadingConstant))
+      }
+
+      if let bottom = bottom {
+        anchors.append(bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant))
+      }
+
+      if let trailing = trailing {
+        anchors.append(trailingAnchor.constraint(equalTo: trailing, constant: -trailingConstant))
+      }
+
+      if widthConstant > 0 {
+        anchors.append(widthAnchor.constraint(equalToConstant: widthConstant))
+      }
+
+      if heightConstant > 0 {
+        anchors.append(heightAnchor.constraint(equalToConstant: heightConstant))
+      }
+
+      anchors.forEach { $0.isActive = true }
+
+      return anchors
+    }
+
+}
