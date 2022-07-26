ac3993ea3affb1ebec2c0ba538c6ef3c63003d67
Sufiyan Yasa <x1337@gmail.com>
2022-07-26 18:08:01 +0800

    Refactor SkeletonDemo packages

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -3,79 +3,123 @@
 
 import PackageDescription
 
+// dependencies
 let swifterSwift = Target.Dependency.product(name: "SwifterSwift", package: "SwifterSwift")
 let swiftCollections = Target.Dependency.product(name: "OrderedCollections", package: "swift-collections")
+let previewView = Target.Dependency.product(name: "PreviewView", package: "PreviewView")
 
-let constants = BuildItem(name: "Constants")
-let darwinNotification = BuildItem(service: "DarwinNotification", dependencies: [constants.asDependency])
-let services = [
-  constants,
-  BuildItem(service: "UserDefaultsBacked", dependencies: [constants.asDependency]),
-  BuildItem(service: "Logger"),
-  darwinNotification,
-  BuildItem(service: "Coordinator"),
-  BuildItem(service: "Persistence", dependencies: [darwinNotification.asDependency]),
-]
+let commonSetup = SetupGroup(items: [
+  "Constants": []
+])
+let serviceGroup = SetupGroup(
+  sourcePath: "Sources/Services",
+  items: [
+    "DarwinNotification": ["Constants"],
+    "UserDefaultsBacked": ["Constants"],
+    "Logger": [],
+    "Coordinator": [],
+    "Persistence": ["DarwinNotification"],
+  ]
+)
+let modelGroup = SetupGroup(
+  sourcePath: "Sources/Models",
+  items: [
+    "Appearance": [swiftCollections]
+  ]
+)
+let featureGroup = SetupGroup(
+  sourcePath: "Sources/Features",
+  items:[
+    "Settings": ["Appearance", swifterSwift]
+  ]
+)
 
-let modelAppearance = BuildItem(model: "Appearance", dependencies:[swiftCollections])
-let models = [
-  modelAppearance
-]
-
-let featureSettings = BuildItem(feature: "Settings", dependencies:[swifterSwift, modelAppearance.asDependency])
-let features = [
-  featureSettings
-]
-
+let allModules = [commonSetup, serviceGroup, modelGroup, featureGroup]
+let products = allModules.flatMap { $0.makeProducts() }
+let targets = allModules.flatMap {$0.makeTargets()} + allModules.flatMap { $0.makeTestTargets() }
 
 let package = Package(
   name: "SkeletonDemo",
   platforms: [
     .iOS(.v15)
   ],
-  products: [services, models, features].flatMap { $0 }.map(\.library),
+  products: products,
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", branch: "master"),
     .package(url: "https://github.com/apple/swift-collections", branch: "main"),
+    .package(url: "https://github.com/theoriginalbit/PreviewView", branch: "main")
   ],
-  targets: [services, features, models].flatMap{ $0 }.map(\.target)
+  targets: targets
 )
 
-struct BuildItem {
-  let name: String
-  let asDependency: Target.Dependency
-  let dependencies: [Target.Dependency]
-  let resources: [Resource]
-  let path: String?
-  
-  init(name: String, path: String? = nil, dependency: Target.Dependency? = nil, dependencies: [Target.Dependency] = [], resources: [Resource] = []) {
-    self.name = name
-    self.path = path
-    self.asDependency = dependency ?? .init(stringLiteral: name)
-    self.dependencies = dependencies
-    self.resources = resources
-  }
-  
-  init(service: String, dependencies: [Target.Dependency] = [], resources: [Resource] = []) {
-    self.init(name: service, path: "Sources/Services/\(service)", dependencies: dependencies, resources: resources)
-  }
-  init(feature: String, dependencies: [Target.Dependency] = [], resources: [Resource] = []) {
-    self.init(name: "Feature\(feature)", path: "Sources/Features/\(feature)", dependencies: dependencies, resources: resources)
-  }
-  init(model: String, dependencies: [Target.Dependency] = [], resources: [Resource] = []) {
-    self.init(name: "Model\(model)", path: "Sources/Models/\(model)", dependencies: dependencies, resources: resources)
-  }
-  
-  var library: Product {
-    Product.library(name: name, targets: [name])
-  }
-  var target: Target {
-    Target.target(name: name, dependencies: dependencies, path: path, resources: resources)
-  }
-  var testTarget: Target {
-    Target.testTarget(name: name, dependencies: dependencies, path: path, resources: resources)
-  }
+// MARK: - Builders
+
+typealias Features = [String: [Target.Dependency]]
+struct SetupGroup: Builder {
+  var prefixPath: String = ""
+  var sourcePath: String = "Sources"
+  var testSourcePath: String = "Tests"
+  var items: Features = [:]
+  var testItems: Features = [:]
+  var mockItems: Features = [:]
 }
 
+protocol Builder {
+  var prefixPath: String { get }
+  var sourcePath: String { get }
+  var testSourcePath: String { get }
+  var items: Features { get }
+  var testItems: Features { get }
+  var mockItems: Features { get }
+}
+
+extension Builder {
+  func makeProducts() -> [Product] {
+    items.keys.map {
+      .library(
+        name: "\(prefixPath)\($0)",
+        targets: ["\(prefixPath)\($0)"])
+    }
+  }
+  
+  func makeTargets() -> [Target] {
+    items.enumerated().map { _, tuple in
+      let name = "\(prefixPath)\(tuple.key)"
+      return Target.target(
+        name: name,
+        dependencies: tuple.value,
+        path: "\(sourcePath)/\(tuple.key)",
+        resources: [])
+    }
+  }
+  
+  func makeTestTargets() -> [Target] {
+    testItems.enumerated().map { _, tuple in
+      let name = "\(prefixPath)\(tuple.key)"
+      return Target.testTarget(
+        name: name,
+        dependencies: tuple.value,
+        path: "\(testSourcePath)/\(tuple.key)",
+        resources: [])
+    }
+  }
+  
+  func makeMockProducts() -> [Product] {
+    mockItems.keys.map {
+      .library(
+        name: "\(prefixPath)\($0)Mock",
+        targets: ["\(prefixPath)\($0)Mock"])
+    }
+  }
+  
+  func makeMockTargets() -> [Target] {
+    mockItems.enumerated().map { _, tuple in
+      let name = "\(prefixPath)\(tuple.key)Mock"
+      return Target.target(
+        name: name,
+        dependencies: tuple.value + [],
+        path: "Tests/Mocks/\(tuple.key)",
+        resources: [])
+    }
+  }
+}
